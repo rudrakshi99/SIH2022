ec41795f67db5cff6b99526e7998c799f303f447
suyashrastogi7 <suyashrastogi@protonmail.com>
2022-02-15 21:21:53 +0530

    Project Structure and tailwind init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Kisaan Equiment Exchange</h1>
+      <h1>Kisaan Equipment Exchange</h1>
     </div>
   );
 }
