8bf342e29909900eefcd2ae81fadd4b745a6a328
Anmol <anmol.srivastava@createbytes.com>
2022-03-02 16:53:03 +0530

    changed default value for reading dot env file

--- config/settings/base.py
+++ config/settings/base.py
@@ -11,7 +11,7 @@ ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 APPS_DIR = ROOT_DIR / "kex"
 env = environ.Env()
 
-READ_DOT_ENV_FILE = env.bool("DJANGO_READ_DOT_ENV_FILE", default=True)
+READ_DOT_ENV_FILE = env.bool("DJANGO_READ_DOT_ENV_FILE", default=False)
 if READ_DOT_ENV_FILE:
     # OS environment variables take precedence over variables from .env
     env.read_env(str(ROOT_DIR / ".envs/.local/.django"))
