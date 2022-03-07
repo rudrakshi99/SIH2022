01903ec17a365612fcc8d82de11962d35e814d57
suyashrastogi7 <suyashrastogi@protonmail.com>
2022-03-07 08:04:03 +0530

    updated help center

--- src/components/expandText/index.js
+++ src/components/expandText/index.js
@@ -1,27 +1,67 @@
 import React, { useState } from "react";
 
-import down_arrow from "../../img/down-arrow.svg";
-
 const ExpandDropdown = ({ heading, content }) => {
   const [open, setOpen] = useState(false);
 
   return (
-    <div className="m-3">
-      <div
-        className=" bg-green-200 px-9 py-3 cursor-pointer hover:bg-green-300 flex"
-        onClick={() => setOpen((prev) => setOpen(!prev))}
-      >
-        <p className="text-xl font-semibold">{heading}</p>
-        <img
-          style={{ height: "20px", width: "20px" }}
-          src={down_arrow}
-          alt="down-arrow"
-          className="ml-auto"
-        />
-      </div>
-      <div className={`${!open && "hidden"}  bg-green-300`}>
-        <p className="px-9 py-3 text-lg">{content}</p>
-      </div>
+    <div>
+      <article className="border-b">
+        <div className="border-l-2 border-transparent">
+          <header
+            className="flex justify-between items-center p-5 pl-8 pr-8 cursor-pointer select-none"
+            onClick={() => setOpen((prev) => !prev)}
+          >
+            <span className="text-grey-darkest font-thin text-xl">
+              {heading}
+            </span>
+            <div className="rounded-full border border-grey w-7 h-7 flex items-center justify-center">
+              <svg
+                ariaHidden="true"
+                className=""
+                dataReactid="266"
+                fill="none"
+                height="24"
+                stroke="#606F7B"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                viewbox="0 0 24 24"
+                width="24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <polyline points="6 9 12 15 18 9"></polyline>
+              </svg>
+            </div>
+          </header>
+        </div>
+      </article>
+      <article className={`${open ? "show" : "hidden"} border-b`}>
+        <div className="border-l-2 bg-grey-lightest border-indigo">
+          <header
+            className="flex justify-between items-center p-5 pl-8 pr-8 cursor-pointer select-none"
+            onClick={() => setOpen(false)}
+          >
+            <span className="text-indigo font-thin text-xl">{content}</span>
+            <div className="rounded-full border border border-indigo w-7 h-7 flex items-center justify-center bg-indigo">
+              <svg
+                ariaHidden="true"
+                data-reactid="281"
+                fill="none"
+                height="24"
+                stroke="white"
+                stroke-linecap="round"
+                stroke-linjoin="round"
+                stroke-width="2"
+                viewbox="0 0 24 24"
+                width="24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <polyline points="18 15 12 9 6 15"></polyline>
+              </svg>
+            </div>
+          </header>
+        </div>
+      </article>
     </div>
   );
 };
