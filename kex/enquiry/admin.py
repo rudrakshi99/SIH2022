6b1386bc048cfc9874e049b40df970df39efd14b
Anmol <anmol.srivastava@createbytes.com>
2022-03-07 11:34:54 +0530

    added feedbackform, report equipment model and apis

--- kex/enquiry/admin.py
+++ kex/enquiry/admin.py
@@ -1,8 +1,29 @@
 from django.contrib import admin
-from kex.enquiry.models import HelpCentre
+from kex.enquiry.models import HelpCentre,FeedbackForm,PartnerDispute,CancelForm,ReportEquipment
 
 # Register your models here.
 @admin.register(HelpCentre)
 class HelpCentre(admin.ModelAdmin):
     list_display = ["name", "phone_number"]
+    search_fields = list_display
+
+@admin.register(PartnerDispute)
+class PartnerDisputeForm(admin.ModelAdmin):
+    list_display = ["name", "phone_number","equipment_id","partner_id"]
+    search_fields = list_display
+
+@admin.register(CancelForm)
+class CancelFormForm(admin.ModelAdmin):
+    list_display = ["user", "booking_id","cancel_reason"]
+    search_fields = list_display
+
+
+@admin.register(ReportEquipment)
+class ReportEquipmentForm(admin.ModelAdmin):
+    list_display = ["user", "equipment","report_reason"]
+    search_fields = list_display
+
+@admin.register(FeedbackForm)
+class FeedbackForm(admin.ModelAdmin):
+    list_display = ["name", "phone_number","description"]
     search_fields = list_display
